ed1bec672671babb1b27fa580cf3d065ec34e29f
Yasi Mortazavii <Mortazaviiyasi@gmail.com>
2022-07-15 14:08:16 +0430

    implement laravel-ide-helper

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
