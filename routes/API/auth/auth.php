162b1d3d159e1c1e72edfde99c7df896c816eaa8
Yasi Mortazavii <Mortazaviiyasi@gmail.com>
2022-07-15 18:45:27 +0430

    Login Steps

--- routes/API/auth/auth.php
+++ routes/API/auth/auth.php
@@ -1,5 +1,8 @@
 <?php
 
+
+
+use App\Http\Controllers\API\auth\LoginController;
 use App\Http\Controllers\API\auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -13,6 +16,15 @@ Route::prefix('v1')->group(function(){
     // API for user active email
     Route::get('activation/{token}', [RegisterController::class, 'signupActive']);
 
+    // API for user login
+    Route::post('login', [LoginController::class, 'login']);
+
+    // Private Routes
+    Route::middleware("auth:api")->group(function () {
+
+    // Show user profile
+        Route::get('profile', [LoginController::class, 'profile']);
+    });
 
 });
 
